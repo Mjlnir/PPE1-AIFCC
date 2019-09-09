168a7d8e3df17ec4ef0b70cb230cc7d88ad2f890
Florian <trudelle.florian@gamil.com>
2019-09-09 18:28:13 +0200

    Update

--- views/navbar.php
+++ views/navbar.php
@@ -7,29 +7,23 @@
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <ul class="navbar-nav">
             <li class="nav-item active" id="home">
-                <a class="nav-link" href="./views/home.php">Accueil</a>
+                <a class="nav-link" href="#">Accueil</a>
             </li>
             <li class="nav-item" id="reservation">
-                <a class="nav-link" href="./views/reservation.php">Réservation</a>
+                <a class="nav-link" href="#">Réservation</a>
             </li>
             <li class="nav-item" id="calendrier">
-                <a class="nav-link" href="./views/calendrier.php">Calendrier</a>
+                <a class="nav-link" href="#">Calendrier</a>
             </li>
             <li class="nav-item" id="user">
-                <a class="nav-link" href="./views/user.php">Mon compte</a>
+                <a class="nav-link" href="#">Mon compte</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="index.php?action=logOut">Déconnexion</a>
             </li>
         </ul>
     </nav>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('a').click(function(){
-            
-        });
-    });
-</script>
+
 <?php
     include("footer.php");
 ?>
