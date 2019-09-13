b87664822659cc9cfe97781c3feeead2e18cd2b7
Mjlnir <trudelle.florian@gmail.com>
2019-09-13 17:27:43 +0200

    Update Calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,33 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         locale: 'fr',
         plugins: ['dayGrid'],
         defaultView: 'dayGridMonth',
         header: {
-            center: 'addEventButton'
+            left: 'title',
+            right: 'prev,next today'
         },
         customButtons: {
             addEventButton: {
                 text: 'Réserver',
                 click: function () {
-                    var dateStr = prompt('Entrer une date au format AAAA-MM-JJ.');
+                    $('#myModal').show();
+                    /*var dateStr = prompt('Entrer une date au format AAAA-MM-JJ.');*/
                     var date = new Date(dateStr + 'T00:00:00');
 
-                    if (!isNaN(date.valueOf())) {
+                    /*if (!isNaN(date.valueOf())) {
                         calendar.addEvent({
                             title: 'dynamic event',
                             start: date,
                             allDay: false
-                        });
-                        alert('Great. Now, update your database...');
-                    } else {
-                        alert('Date invalide.');
-                    }
+                        });*/
                 }
             }
         }
     });
-
     calendar.render();
+    $('.fc-day').click(function () {
+        $('#myModal').show();
+    });
+    $('.fc-today-button, .fc-prev-button, .fc-next-button, .fc-dayGridMonth-button, .fc-timeGridWeek-button, .fc-timeGridDay-button').click(function () {
+        $('.fc-day:none(:fc-other-month)').click(function () {
+            $('#myModal').show();
+        });
+    });
+    
+    $('.closeMdl').click(function(){
+        $('#myModal').hide();
+    });
+    $('.saveMdl').click(function(){
+        $('#myModal').hide();
+    });
 });
--- views/calendrier.php
+++ views/calendrier.php
@@ -1,17 +1,17 @@
 <?php
 	include("header.php");
 ?>
-    <link rel="stylesheet" type="text/css" href="./public/css/calendrier.css">
+<link rel="stylesheet" type="text/css" href="./public/css/calendrier.css">
 
-    <link href='./public/fullCalendar/packages/core/main.css' rel='stylesheet' />
-    <link href='./public/fullCalendar/packages/daygrid/main.css' rel='stylesheet' />
-    <!--<link href='./public/fullCalendar/packages/timegrid/main.css' rel='stylesheet' />-->
+<link href='./public/fullCalendar/packages/core/main.css' rel='stylesheet' />
+<link href='./public/fullCalendar/packages/daygrid/main.css' rel='stylesheet' />
+<link href='./public/fullCalendar/packages/timegrid/main.css' rel='stylesheet' />
 
-    <script src='./public/fullCalendar/packages/core/main.js'></script>
-    <script src='./public/fullCalendar/packages/core/locales/fr.js'></script>
-    <script src='./public/fullCalendar/packages/daygrid/main.js'></script>
-    <!--<script src='./public/fullCalendar/packages/timegrid/main.js'></script>-->
-    <script src='./public/js/calendar.js'></script>
+<script src='./public/fullCalendar/packages/core/main.js'></script>
+<script src='./public/fullCalendar/packages/core/locales/fr.js'></script>
+<script src='./public/fullCalendar/packages/daygrid/main.js'></script>
+<script src='./public/fullCalendar/packages/timegrid/main.js'></script>
+<script src='./public/js/calendar.js'></script>
 </head>
 
 <body class="d-flex flex-column">
@@ -20,6 +20,28 @@
     ?>
     <div id="page-content">
         <div id="calendar"></div>
+    </div>
+    <div class="modal" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Réserver une salle</h4>
+                    <button type="button" class="close closeMdl" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    Modal body..
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger saveMdl" data-dismiss="modal">Réserver</button>
+                </div>
+            </div>
+        </div>
     </div>
     <?php
         include("footer.php");
